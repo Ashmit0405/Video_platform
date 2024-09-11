599df6bcdd302d6646b71d0c8c145793556998b0
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2024-09-12 03:18:16 +0530

    Adding Like controllers

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,81 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { asyncHandler } from "../utils/asyncfunction.js";
+import { ApiError } from "../utils/apiError.js";
+import { Like } from "../models/like.model.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+
+const togglev_likes=asyncHandler(async (req,res)=>{
+    const {v_id}=req.params;
+    if(!isValidObjectId(v_id)){
+        throw new ApiError(404,"Invalid id")
+    }
+
+    const likedstatus= await Like.findOne({
+        video: v_id,
+        likedby: req.user?._id
+    })
+
+    if(likedstatus){
+        await Like.findByIdAndDelete(likedstatus?._id)
+        return res.status(200).json(new ApiResponse(200,"Liked Removed Successfully"));
+    }
+
+    const test=await Like.create({
+        video: v_id,
+        likedby: req.user?._id
+    })
+
+    return res.status(200).json(new ApiResponse(200,test,"Liked Added Successfully"))
+})
+
+const togglec_likes=asyncHandler(async(req,res)=>{
+    const {c_id}=req.params;
+    if(!isValidObjectId(c_id)){
+        throw new ApiError(404,"Comment not found")
+    }
+
+    const likestatus=await Like.findOne({
+        comment: c_id,
+        likedby: req.user?._id
+    })
+
+    if(likestatus){
+        const test=await Like.findByIdAndDelete(likestatus?._id)
+        return res.status(200).json(new ApiResponse(200,test,"Likes Removed Successfully"))
+    }
+
+    const test=await Like.create({
+        comment: c_id,
+        likedby: req.user?._id
+    })
+
+    return res.status(200).json(new ApiResponse(200,test,"Comment Liked successfully"))
+})
+
+const togglet_likes=asyncHandler(async(req,res)=>{
+    const {t_id}=req.params
+    if(!isValidObjectId(t_id)){
+        throw new ApiError(404,"Tweet Not found");
+    }
+
+    const likedstatus=await Like.findOne({
+        tweet: t_id,
+        likedby: req.user?._id
+    })
+
+    if(likedstatus){
+        await Like.findByIdAndDelete(likedstatus?._id);
+        return res.status(200).json(new ApiResponse(200,"Liked Removed Successfully"));
+    }
+
+    await Like.create({
+        tweet: t_id,
+        likedby: req.user?._id
+    })
+
+    return res.status(200).json(new ApiResponse(200,"Liked Successfully"));
+})
+
+const getliked_videos=asyncHandler(async(req,res)=>{
+    
+})
