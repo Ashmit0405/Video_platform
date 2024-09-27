81a3cff23ff88ac2505ba6cc5da631899e91933b
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2024-09-27 14:35:27 +0530

    Added the likes controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -77,5 +77,66 @@ const togglet_likes=asyncHandler(async(req,res)=>{
 })
 
 const getliked_videos=asyncHandler(async(req,res)=>{
-    
+    const likedvideo=await Like.aggregate([
+        {
+            $match:{
+                likedby: mongoose.Types.ObjectId(req.user?._id),
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "liked_video"
+            },
+            pipeline:[
+                {
+                    $lookup:{
+                        from: "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner"
+                    },
+                },
+                {
+                    $unwind: "$owner",   
+                }
+            ],
+        },
+        {
+            $unwind: "$liked_video",
+        },
+        {
+            $sort:{
+                createdAt:-1
+            }
+        },
+        {
+            $project:{
+                $id: 0,
+                liked_video:{
+                    _id: 1,
+                    "videoFile.url":1,
+                    "thumbnail.url":1,
+                    owner: 1,
+                    title: 1,
+                    description: 1,
+                    duration: 1,
+                    views: 1,
+                    isPublished: 1,
+                    createdAt: 1,
+                    owner:{
+                        username: 1,
+                        fullname: 1,
+                        "avatar.url": 1
+                    }
+                }
+            }
+        }
+    ]);
+
+    return res.status(200).json(new ApiResponse(200,likedvideo,"Liked Videos fetched successfully"));
 })
+
+export {getliked_videos,togglec_likes,togglet_likes,togglev_likes};
