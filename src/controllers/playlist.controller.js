e1e977f118e11bde1129420d96221296710eca21
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2024-10-08 18:11:07 +0530

    Added remove and add video functions

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -2,6 +2,7 @@ import mongoose, { isValidObjectId } from "mongoose";
 import { asyncHandler } from "../utils/asyncfunction";
 import { ApiError } from "../utils/apiError";
 import { Playlist } from "../models/playlist.model";
+import { Video } from "../models/video.model";
 import { ApiResponse } from "../utils/apiResponse";
 
 
@@ -83,5 +84,69 @@ const deleteplaylist=asyncHandler(async(req,res)=>{
 });
 
 const addvideo=asyncHandler(async(req,res)=>{
-    
+    const {playlistid,vid}=req.params;
+
+    if(!isValidObjectId(playlistid)||!isValidObjectId(vid)){
+        throw new ApiError(400,"Playlist or video not found");
+    }
+
+    const playlist=await Playlist.findById(playlistid);
+    const video=await Video.findById(vid);
+    if(!playlist){
+        throw new ApiError(400,"Playlist Not Found");
+    }
+    if(!video){
+        throw new ApiError(400,"Video not found");
+    }
+
+    if(playlist.owner?.toString()&&video.owner?.toString()!==req.user?._id.toString()){
+        throw new ApiError(400,"Only Owner can make changes");
+    }
+
+    const updatedplaylist=await Playlist.findByIdAndUpdate(playlistid,{
+        $addToSet:{
+            videos: vid
+        },
+    },{
+        new: true
+    })
+
+    if(!updatedplaylist){
+        throw new ApiError(400,"Error updating playlist")
+    }
+
+    return res.status(200).json(new ApiResponse(200,updatedplaylist,"Playlist updated successfully"));
 })
+
+const removevideo=asyncHandler(async(req,res)=>{
+    const {playlistid,vid}=req.params;
+    if(!isValidObjectId(playlistid)||!isValidObjectId(vid)){
+        throw new ApiError(400,"Playlist or video not found");
+    }
+
+    const playlist=await Playlist.findById(playlistid);
+    const video=await Video.findById(vid);
+    if(!playlist){
+        throw new ApiError(400,"Playlist Not Found");
+    }
+    if(!video){
+        throw new ApiError(400,"Video not found");
+    }
+    if(playlist.owner?.toString()&&video.owner?.toString()!==req.user?._id.toString()){
+        throw new ApiError(400,"Only Owner can make changes");
+    }
+
+    const updatedplaylist=await Playlist.findByIdAndUpdate(playlistid,{
+        $pull:{
+            videos: vid
+        },
+    },{
+        new: true
+    })
+
+    if(!updatedplaylist){
+        throw new ApiError(404,"Error removing video");
+    }
+    res.status(200).json(new ApiResponse(200,updatedplaylist,"Video Removed Successfully"));
+})
+
